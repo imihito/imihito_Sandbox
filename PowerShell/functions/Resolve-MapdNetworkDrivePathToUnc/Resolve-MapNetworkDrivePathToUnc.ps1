c29c764f4feb1f656f65a6a365bf1d726caac689
imihito <pdsoxapq@gmail.com>
2019-12-29 16:38:12 +0900

    add comment.

--- PowerShell/functions/Resolve-MapdNetworkDrivePathToUnc/Resolve-MapNetworkDrivePathToUnc.ps1
+++ PowerShell/functions/Resolve-MapdNetworkDrivePathToUnc/Resolve-MapNetworkDrivePathToUnc.ps1
@@ -6,13 +6,14 @@ function Resolve-MapNetworkDrivePathToUnc {
 .SYNOPSIS
 割り当てられたネットワークドライブのパスをUNCパスに解決します。
 .DESCRIPTION
-Long description
-
-.EXAMPLE
-An example
-
-.NOTES
-General notes
+Resolve-MapNetworkDrivePathToUnc 関数は、割り当てられたネットワークドライブのパスをUNCパスに解決します。
+指定されたパスが相対パスの場合は、絶対パスに変換した上で解決が行われます。
+すでにUNCパスの場合、あるいはローカルパスの場合は絶対パスを返します。
+存在しているパスを指定する必要があります。
+.INPUTS
+System.String
+.OUTPUTS
+System.String
 #>
     [CmdletBinding(
         DefaultParameterSetName = 'LiteralPath',
@@ -20,7 +21,7 @@ General notes
     )]
     [OutputType([string])]
     param (
-        # 
+        # 解決するパスを指定します。LiteralPath の値は、入力した内容のまま使用されます。ワイルドカードとして解釈される文字はありません。
         [Parameter(
             Mandatory = $true, 
             Position = 0,
@@ -32,7 +33,7 @@ General notes
         [Alias('PSPath', 'ProviderPath', 'FullName', 'LocalPath')]
         [string[]]$LiteralPath
         ,
-        # Specifies a path to one or more locations.
+        # 解決するパスを指定します。ワイルドカードを使用できます。
         [Parameter(
             Mandatory = $true,
             ParameterSetName = 'Path'
@@ -43,27 +44,25 @@ General notes
     )
     begin {
         # 割り当てられたネットワークドライブの一覧をDictionaryに格納。
-        [Dictionary[string, string]]$driveUncDic = 
-            # 念のため大文字・小文字無視の辞書とする
-            [Dictionary[string, string]]::new([System.StringComparer]::InvariantCultureIgnoreCase)
+        [Dictionary[string, string]]$driveUncDic = [Dictionary[string, string]]::new()
+        
         # WMI 経由でドライブ情報を取得。
         # DeviceID が ドライブレター、ProviderName が 実際のパスとなる。
-        Get-CimInstance -Query '
-            SELECT 
-                DeviceID, ProviderName 
-            FROM 
-                Win32_MappedLogicalDisk' | 
+        [string]$Wql = 'SELECT DeviceID, ProviderName FROM Win32_MappedLogicalDisk'
+        Get-CimInstance -Query $Wql | 
             ForEach-Object -Process {
                 $driveUncDic.Add($_.DeviceID, $_.ProviderName)
                 $_.Dispose()
             }
     }
     process {
+        # Resolve-Path コマンドレットに、
+        # LiteralPath が指定されたときには $LiteralPath、Path が指定されたときは $Path を渡したいため、スプラッティングを使用する。
         [hashtable]$resolvePathParam = @{
             $($PSCmdlet.ParameterSetName) = $PSBoundParameters[$PSCmdlet.ParameterSetName]
         }
         [PathInfo]$p = $null
-        foreach ($p in Resolve-Path @resolvePathParam -ErrorAction Stop) {
+        foreach ($p in Resolve-Path @resolvePathParam) {
             if ($p.Provider.Name -ne 'FileSystem') {
                 # ファイル以外のものが指定された場合はエラーを出してスキップ。
                 $PSCmdlet.WriteError([ErrorRecord]::new(
@@ -78,25 +77,25 @@ General notes
             [uri]$u = [uri]($p.ProviderPath)
             [string]$localPath = $u.LocalPath # この後よく使うので一旦変数に格納。
 
-            if (-not $u.IsFile) {
-                # ファイル以外ならそのまま
-                $PSCmdlet.WriteObject($localPath)
-                continue
-            }
             if ($u.IsUnc) {
+                # すでにUNCパスならそのままでOK。
                 $PSCmdlet.WriteObject($localPath)
                 continue
             }
             
+            # continue の対象がわかりにくくになるため、foreach ではなく、ForEach-Object を使用。
             $driveUncDic.GetEnumerator() |
                 ForEach-Object -Process {
+                    # Resolve-Path によりパスの大文字小文字は正規化されているので、そのまま比較してOK。
                     if ($localPath.StartsWith($_.Key)) {
+                        # パスの先頭がドライブレターなら、ドライブレター以降を切り出して実際のパスと結合。
                         [string]$resolvedPath = $_.Value + $localPath.Substring($_.Key.Length)
                         $PSCmdlet.WriteObject($resolvedPath)
                         continue
                     }
                 }
             
+            # ローカルのパス(C ドライブとか)
             $PSCmdlet.WriteObject($localPath)
         }
     }
